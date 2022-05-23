a40da1777b2a75a10dd769f5a3f955ef57d59e4f
SupremeSpark <bautistaj697@yahoo.com>
2022-05-22 16:27:32 -1000

    Settings | alpha-0.0.17
    - Resolution dropdown works now
    
    New bugs found from LAST build:
    - Start button does not load 'Game' scene more than once

--- TimeShip (2023)/Assets/SettingsMenu.cs
+++ TimeShip (2023)/Assets/SettingsMenu.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     private float visualVolume;
     [SerializeField] private TextMeshProUGUI volumeTXT;
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionsDropdown;
 
     public void Start(){
+//sets recommedned volume
         audioMixer.SetFloat("MasterVolume", -20);
+//resolution stuff
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        //loops for available resolutions
+        for (int i = 0; i < resolutions.Length; i++){
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            //finds recommedned resolution
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+                currentResolutionIndex = i;
+            }
+        }
+
+        //adds a vailable resolutions
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.RefreshShownValue();
+        //sets recommedned resolution
+        resolutionsDropdown.value = currentResolutionIndex;
     }
 
     public void SetVolume (float volume){
@@ -24,5 +50,9 @@ public class SettingsMenu : MonoBehaviour
     }
     public void SetFullscreen (bool isFullscreen){
        Screen.fullScreen = isFullscreen;
+    }
+    public void SetResolution (int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
